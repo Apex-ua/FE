f3bbc5c65baa354f79a65da1e752b50055b9e046
Иван <ivan@Ivans-MBP.Dlink>
2019-03-31 23:44:29 +0300

    test

---
+++ table.js
@@ -0,0 +1,34 @@
+{
+    tables: [
+    {
+        id: "1",
+        places: 4,
+        location: {
+        x: 1,
+        y: 2
+        }
+    }
+    ]
+}
+
+{
+    bookings: [
+        {
+            "tableId": "1",
+            "startTime": "2019-03-29 10:00",
+            "endTime": "2019-03-29 12:00",
+            "customerName": "Apex",
+            "customerPhone": "063..."
+        }
+    ]
+}
+
+
+{
+    "name": "Вася",
+    "age": 25,
+    "roles": {
+        "isAdmin": false,
+        "isEditor": true
+    }
+}
